b7f96e9551ab595765c526f12753c03fcdb95213
Diego Lemos <55662765+scanak47@users.noreply.github.com>
2025-10-08 10:18:26 -0300

    Delete entregar.py

--- entregar.py
+++
@@ -1,8 +0,0 @@
-
-qtdlanche= int(input("Quantidade de lanche"))
-qtdsalgado= int(input("Quantidade de salgado"))
-qtdrefri= int(input("Quantidade de refrigerante"))
-
-total= (qtdlanche *15) + (qtdsalgado * 5) + (qtdrefri *6)
-
-print(f" Valor total da conta :R$ {total: .2f}")
